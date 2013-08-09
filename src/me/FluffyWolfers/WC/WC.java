bd31b353f4625aa5c936b4b5447728b654114e8f
FluffyNarwhals <fluffynarwhalsgaming@gmail.com>
2013-08-09 16:07:17 -0700

    Created plugin. Finished main plugin.

---
+++ src/me/FluffyWolfers/WC/WC.java
@@ -0,0 +1,23 @@
+package me.FluffyWolfers.WC;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class WC extends JavaPlugin{
+
+	public static WC w;
+	public static PluginDescriptionFile pdf;
+
+	public void onEnable(){
+
+		w = this;
+		pdf = this.getDescription();
+		
+		Bukkit.getLogger().info("[WolfColors v" + pdf.getVersion() + "] Starting up...");
+
+		this.getServer().getPluginManager().registerEvents(new WCListener(), this);
+
+	}
+
+}
---
+++ src/me/FluffyWolfers/WC/WCListener.java
@@ -0,0 +1,107 @@
+package me.FluffyWolfers.WC;
+
+import java.util.ArrayList;
+
+import org.bukkit.ChatColor;
+import org.bukkit.DyeColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Wolf;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+
+@SuppressWarnings("deprecation")
+public class WCListener implements Listener{
+	
+	Entity en;
+	
+	ArrayList<Player> pla = new ArrayList<Player>();
+	
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void clickWolf(PlayerInteractEntityEvent e){
+		
+		Player p = e.getPlayer();
+		Entity clicked = e.getRightClicked();
+		
+		if(clicked instanceof Wolf){
+			
+			if(((Wolf) clicked).isTamed()){
+				
+				if(((Wolf) clicked).getOwner().equals(p)){
+
+					en = clicked;
+					
+				}else{
+					p.sendMessage(ChatColor.DARK_RED + "Not yours!");
+					return;
+				}
+				
+			}else{
+				p.sendMessage(ChatColor.DARK_RED + "Not tamed!");
+				return;
+			}
+			
+		}else{
+			p.sendMessage(ChatColor.DARK_RED + "Not a wolf!");
+			return;
+		}
+		
+		if(p.getItemInHand().getType().equals(Material.STICK)){
+			
+			pla.add(p);
+			
+			p.sendMessage(ChatColor.GOLD + "Choose a color: " + ChatColor.BLUE + "blue, " + ChatColor.LIGHT_PURPLE + "pink, " + ChatColor.DARK_GREEN + "green, " + ChatColor.GREEN + "lime, " + ChatColor.GOLD + "orange, " + ChatColor.RED + "red, " + ChatColor.YELLOW + "yellow, or" + ChatColor.DARK_PURPLE + "purple");
+			
+		}
+		
+	}
+	
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onChat(PlayerChatEvent e){
+		
+		Player p = e.getPlayer();
+		String msgStr = e.getMessage();
+		String msg = msgStr.toLowerCase();
+		
+		if(pla.contains(p)){
+			
+			e.setCancelled(true);
+			
+			if(en instanceof Wolf){
+				
+				Wolf wolf = (Wolf) en;
+				
+				if(msg.equalsIgnoreCase("blue")){
+					wolf.setCollarColor(DyeColor.BLUE);
+				}else if(msg.equalsIgnoreCase("green")){
+					wolf.setCollarColor(DyeColor.GREEN);
+				}else if(msg.equalsIgnoreCase("red")){
+					wolf.setCollarColor(DyeColor.RED);
+				}else if(msg.equalsIgnoreCase("yellow")){
+					wolf.setCollarColor(DyeColor.YELLOW);
+				}else if(msg.equalsIgnoreCase("aqua")){
+					wolf.setCollarColor(DyeColor.LIGHT_BLUE);
+				}else if(msg.equalsIgnoreCase("purple")){
+					wolf.setCollarColor(DyeColor.PURPLE);
+				}else if(msg.equalsIgnoreCase("lightgreen")){
+					wolf.setCollarColor(DyeColor.LIME);
+				}else if(msg.equalsIgnoreCase("orange")){
+					wolf.setCollarColor(DyeColor.ORANGE);
+				}else if(msg.equalsIgnoreCase("pink")){
+					wolf.setCollarColor(DyeColor.PINK);
+				}else{
+					p.sendMessage(ChatColor.DARK_RED + "Not a color! Please choose again!");
+					p.sendMessage(ChatColor.GOLD + "Choose a color: " + ChatColor.BLUE + "blue, " + ChatColor.LIGHT_PURPLE + "pink, " + ChatColor.DARK_GREEN + "green, " + ChatColor.GREEN + "lime, " + ChatColor.GOLD + "orange, " + ChatColor.RED + "red, " + ChatColor.YELLOW + "yellow, or" + ChatColor.DARK_PURPLE + "purple");
+				}
+				
+			}
+			
+		}
+		
+	}
+	
+}
